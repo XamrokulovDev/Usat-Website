82a75eb69160a489b78596eb337ff023869876ca
Ulfatjon <frontenduchun@gmail.com>
2025-07-28 18:51:12 +0500

    The project create PopularNews and News components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@ import Achievements from "../components/Achievements";
 import Contact from "../components/Contact";
 import Header from "../components/Header";
 import Kafedra from "../components/Kafedra";
+import News from "../components/News";
+import PopularNews from "../components/PopularNews";
 import TeamSection from "../components/TeamSection";
 
 const Home = () => {
@@ -14,6 +16,8 @@ const Home = () => {
       <TeamSection />
       <Achievements />
       <Contact />
+      <PopularNews />
+      <News />
     </div>
   );
 };
